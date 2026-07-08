cbd252c70ae6669ed595fd8108938060c5c2803c
Web Yatey <professorsimon622@gmail.com>
2026-07-08 16:23:44 +0000

    Add files via upload
    Local Host database Connection

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$dbname = "flood_monitoring";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Optional: Set UTF-8 character set
+$conn->set_charset("utf8mb4");
+?>
